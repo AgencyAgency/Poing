537aef4a54981ac79ce720b5c6f71a0bc8c3c889
Kyle Oba <koba@mudphone.com>
2013-12-21 16:37:47 -0800

    add new date picker

---
+++ Poing/Poing/AACheckScheduleVC.h
@@ -0,0 +1,13 @@
+//
+//  AACheckScheduleVC.h
+//  Poing
+//
+//  Created by Kyle Oba on 12/21/13.
+//  Copyright (c) 2013 AgencyAgency. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AACheckScheduleVC : UIViewController
+
+@end
---
+++ Poing/Poing/AACheckScheduleVC.m
@@ -0,0 +1,29 @@
+//
+//  AACheckScheduleVC.m
+//  Poing
+//
+//  Created by Kyle Oba on 12/21/13.
+//  Copyright (c) 2013 AgencyAgency. All rights reserved.
+//
+
+#import "AACheckScheduleVC.h"
+
+@interface AACheckScheduleVC ()
+@property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
+
+@end
+
+@implementation AACheckScheduleVC
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)configureView
+{
+    
+}
+
+@end
